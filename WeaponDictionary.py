20b985d3868cadb7ca54da2263c3e6516cf1bc5a
D238252 <cdavis91@horizon.csueastbay.edu>
2022-05-10 01:42:06 -0700

    Commit/Push for addition of potential hashindex function

--- WeaponDictionary.py
+++ WeaponDictionary.py
@@ -1,9 +1,17 @@
-
-
-#Doin the hashtable sha blam
+# Doin the hashtable sha blam
 import random
 
 x = random.randrange(0,10)
 
 print(x)
 
+
+def hashindex(elem):
+    total = 0
+    for i in elem:
+        total += ord(i)
+    return total
+
+sample = "Short Sword"
+
+print(hashindex(sample))
